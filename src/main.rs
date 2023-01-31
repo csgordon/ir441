08bfbbf4ab9856c640a9239fd6e34aaeefc39d6e
Colin S. Gordon <csgordon@drexel.edu>
2023-01-31 14:04:50 -0500

    fix unused paren warning

--- src/main.rs
+++ src/main.rs
@@ -7,56 +7,69 @@ use std::io::prelude::*;
 use std::io::{self, BufReader};
 use std::path::Path;
 
-use std::str::{from_utf8};
-use nom::{Finish};
+use nom::Finish;
+use std::str::from_utf8;
 
+use crate::ir441::exec::*;
 use crate::ir441::nodes::*;
 use crate::ir441::parsing::*;
-use crate::ir441::exec::*;
-
-
-
-
-
 
 fn check_warnings(prog: &IRProgram) {
     if !prog.blocks.contains_key("main") {
         println!("WARNING: No main block found");
     }
-    for (_,b) in prog.blocks.iter() {
+    for (_, b) in prog.blocks.iter() {
         let mut past_phis = false;
         for i in b.instrs.iter() {
             // Check that all phi targets exist
             match i {
                 IRStatement::Phi { .. } => {
                     if past_phis {
-                        println!("ERROR: phi instruction after non-phis in basic block {}: {:?}", b.name, i);
+                        println!(
+                            "ERROR: phi instruction after non-phis in basic block {}: {:?}",
+                            b.name, i
+                        );
                     }
-                },
-                _ => (past_phis = true)
+                }
+                _ => past_phis = true,
             }
         }
         match &b.next {
-            ControlXfer::If { cond: _, tblock:t, fblock:f } => {
+            ControlXfer::If {
+                cond: _,
+                tblock: t,
+                fblock: f,
+            } => {
                 if !prog.blocks.contains_key(t) {
-                    println!("ERROR: next block |{}| in block {} does not exist!", t, b.name);
+                    println!(
+                        "ERROR: next block |{}| in block {} does not exist!",
+                        t, b.name
+                    );
                 }
                 if !prog.blocks.contains_key(f) {
-                    println!("ERROR: next block |{}| in block {} does not exist!", f, b.name);
+                    println!(
+                        "ERROR: next block |{}| in block {} does not exist!",
+                        f, b.name
+                    );
                 }
             }
-            ControlXfer::Jump { block:l } => {
+            ControlXfer::Jump { block: l } => {
                 if !prog.blocks.contains_key(l) {
-                    println!("ERROR: next block |{}| in block {} does not exist!", l, b.name);
+                    println!(
+                        "ERROR: next block |{}| in block {} does not exist!",
+                        l, b.name
+                    );
                 }
             }
-            _ => ()
+            _ => (),
         }
     }
 }
 
-fn main() -> Result<(),Box<dyn std::error::Error>> {
-    let cmd = std::env::args().nth(1).expect("need subcommand [check|exec|trace|perf]");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cmd = std::env::args()
+        .nth(1)
+        .expect("need subcommand [check|exec|trace|perf]");
     let txt = std::env::args().nth(2);
     let mut reader: Box<dyn BufRead> = match txt {
         None => Box::new(BufReader::new(io::stdin())),
@@ -70,9 +83,11 @@ fn main() -> Result<(),Box<dyn std::error::Error>> {
         }
     };
 
-    let mut bytes : Vec<u8> = vec![];
-    reader.read_to_end(&mut bytes)?; 
-    let (_leftover,prog) = parse_program(&bytes[..]).finish().map_err(|nom::error::Error { input, code: _ }| from_utf8(input).unwrap())?;
+    let mut bytes: Vec<u8> = vec![];
+    reader.read_to_end(&mut bytes)?;
+    let (_leftover, prog) = parse_program(&bytes[..])
+        .finish()
+        .map_err(|nom::error::Error { input, code: _ }| from_utf8(input).unwrap())?;
     let cmd_str = cmd.as_str();
 
     let mut cycles = ExecStats::new();
@@ -87,15 +102,20 @@ fn main() -> Result<(),Box<dyn std::error::Error>> {
     } else if cmd_str == "exec-fixedmem" {
         println!("Parsed: {}", prog);
         check_warnings(&prog);
-        let _fresult = run_prog(&prog, true, &mut cycles, ExecMode::MemCap {limit:100});
+        let _fresult = run_prog(&prog, true, &mut cycles, ExecMode::MemCap { limit: 100 });
     } else if cmd_str == "exec-gc" {
         println!("Parsed: {}", prog);
         check_warnings(&prog);
-        let _fresult = run_prog(&prog, false, &mut cycles, ExecMode::GC {limit:100});
+        let _fresult = run_prog(&prog, false, &mut cycles, ExecMode::GC { limit: 100 });
     } else if cmd_str == "exec-gc-logging" {
         println!("Parsed: {}", prog);
         check_warnings(&prog);
-        let _fresult = run_prog(&prog, false, &mut cycles, ExecMode::LoggingGC {limit:100});
+        let _fresult = run_prog(
+            &prog,
+            false,
+            &mut cycles,
+            ExecMode::LoggingGC { limit: 100 },
+        );
     } else if cmd_str == "trace" {
         println!("Parsed: {}", prog);
         check_warnings(&prog);
@@ -106,110 +126,198 @@ fn main() -> Result<(),Box<dyn std::error::Error>> {
         let _ = run_prog(&prog, false, &mut cycles, ExecMode::Unlimited);
         println!("Execution stats:\n{:?}", cycles);
     } else {
-        println!("Unsupported command (possibly not-yet-implemented): {}", cmd);
+        println!(
+            "Unsupported command (possibly not-yet-implemented): {}",
+            cmd
+        );
         panic!("Usage: ir441 (check|exec|exec-fixedmem|exec-gc|exec-gc-logging|trace|perf)");
     }
-    
+
     Ok(())
 }
 
 #[cfg(test)]
 mod systests {
-    use std::path::Path;
-    use std::fs::File;
-    use std::io::{BufReader,BufRead};
+    use crate::ir441::exec::*;
     use crate::ir441::nodes::*;
     use crate::ir441::parsing::*;
-    use crate::ir441::exec::*;
-    use std::str::{from_utf8};
-    use nom::{Finish};
+    use nom::Finish;
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+    use std::path::Path;
+    use std::str::from_utf8;
 
-    fn load_program(filepath: &str) -> Result<Vec<u8>,Box<dyn std::error::Error>> {
+    fn load_program(filepath: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
         let libfile = Path::new(&filepath);
         let display = libfile.display();
-        let mut reader: Box<dyn BufRead> = 
-        match File::open(&filepath) {
+        let mut reader: Box<dyn BufRead> = match File::open(&filepath) {
             Err(why) => panic!("Couldn't open {}: {}", display, why),
             Ok(file) => Box::new(BufReader::new(file)),
         };
-        let mut bytes : Vec<u8> = vec![];
-        reader.read_to_end(&mut bytes)?; 
+        let mut bytes: Vec<u8> = vec![];
+        reader.read_to_end(&mut bytes)?;
         Ok(bytes)
     }
-    fn parse(bytes:&Vec<u8>) -> Result<IRProgram,Box<dyn std::error::Error>> {
-        let (_leftover,prog) = parse_program(&bytes[..]).finish().map_err(|nom::error::Error { input, code: _ }| from_utf8(input).unwrap())?;
+    fn parse(bytes: &Vec<u8>) -> Result<IRProgram, Box<dyn std::error::Error>> {
+        let (_leftover, prog) = parse_program(&bytes[..])
+            .finish()
+            .map_err(|nom::error::Error { input, code: _ }| from_utf8(input).unwrap())?;
         Ok(prog)
     }
     #[test]
-    fn check_trivial() -> Result<(),Box<dyn std::error::Error>>{
+    fn check_trivial() -> Result<(), Box<dyn std::error::Error>> {
         let bytes = load_program("examples/trivial.ir")?;
         let prog = parse(&bytes)?;
         let mut cycles = ExecStats::new();
         let result = run_prog(&prog, false, &mut cycles, ExecMode::Unlimited);
-        assert_eq!(result,Ok(VirtualVal::Data { val: 23 }));
+        assert_eq!(result, Ok(VirtualVal::Data { val: 23 }));
         Ok(())
     }
     #[test]
-    fn check_countdown() -> Result<(),Box<dyn std::error::Error>>{
+    fn check_countdown() -> Result<(), Box<dyn std::error::Error>> {
         let bytes = load_program("examples/countdown.ir")?;
         let prog = parse(&bytes)?;
-        let mut cycles = ExecStats { allocs: 0, calls: 0, fast_alu_ops: 0, slow_alu_ops: 0, phis: 0, conditional_branches: 0, unconditional_branches: 0, mem_reads: 0, mem_writes: 0, prints: 0, rets: 0 };
+        let mut cycles = ExecStats {
+            allocs: 0,
+            calls: 0,
+            fast_alu_ops: 0,
+            slow_alu_ops: 0,
+            phis: 0,
+            conditional_branches: 0,
+            unconditional_branches: 0,
+            mem_reads: 0,
+            mem_writes: 0,
+            prints: 0,
+            rets: 0,
+        };
         let result = run_prog(&prog, false, &mut cycles, ExecMode::Unlimited);
-        assert_eq!(result,Ok(VirtualVal::Data { val: 0 }));
+        assert_eq!(result, Ok(VirtualVal::Data { val: 0 }));
         Ok(())
     }
     #[test]
-    fn check_basicoo() -> Result<(),Box<dyn std::error::Error>>{
+    fn check_basicoo() -> Result<(), Box<dyn std::error::Error>> {
         let bytes = load_program("examples/basicoo.ir")?;
         let prog = parse(&bytes)?;
-        let mut cycles = ExecStats { allocs: 0, calls: 0, fast_alu_ops: 0, slow_alu_ops: 0, phis: 0, conditional_branches: 0, unconditional_branches: 0, mem_reads: 0, mem_writes: 0, prints: 0, rets: 0 };
+        let mut cycles = ExecStats {
+            allocs: 0,
+            calls: 0,
+            fast_alu_ops: 0,
+            slow_alu_ops: 0,
+            phis: 0,
+            conditional_branches: 0,
+            unconditional_branches: 0,
+            mem_reads: 0,
+            mem_writes: 0,
+            prints: 0,
+            rets: 0,
+        };
         let result = run_prog(&prog, false, &mut cycles, ExecMode::Unlimited);
-        assert_eq!(result,Ok(VirtualVal::Data { val: 3 }));
+        assert_eq!(result, Ok(VirtualVal::Data { val: 3 }));
         Ok(())
     }
     #[test]
-    fn check_gctest1() -> Result<(),Box<dyn std::error::Error>>{
+    fn check_gctest1() -> Result<(), Box<dyn std::error::Error>> {
         let bytes = load_program("examples/gctest1.ir")?;
         let prog = parse(&bytes)?;
-        let mut cycles = ExecStats { allocs: 0, calls: 0, fast_alu_ops: 0, slow_alu_ops: 0, phis: 0, conditional_branches: 0, unconditional_branches: 0, mem_reads: 0, mem_writes: 0, prints: 0, rets: 0 };
+        let mut cycles = ExecStats {
+            allocs: 0,
+            calls: 0,
+            fast_alu_ops: 0,
+            slow_alu_ops: 0,
+            phis: 0,
+            conditional_branches: 0,
+            unconditional_branches: 0,
+            mem_reads: 0,
+            mem_writes: 0,
+            prints: 0,
+            rets: 0,
+        };
         let result = run_prog(&prog, false, &mut cycles, ExecMode::GC { limit: 100 });
-        assert_eq!(result,Ok(VirtualVal::Data { val: 0 }));
+        assert_eq!(result, Ok(VirtualVal::Data { val: 0 }));
         Ok(())
     }
     #[test]
-    fn check_gctest2() -> Result<(),Box<dyn std::error::Error>>{
+    fn check_gctest2() -> Result<(), Box<dyn std::error::Error>> {
         let bytes = load_program("examples/gctest2.ir")?;
         let prog = parse(&bytes)?;
-        let mut cycles = ExecStats { allocs: 0, calls: 0, fast_alu_ops: 0, slow_alu_ops: 0, phis: 0, conditional_branches: 0, unconditional_branches: 0, mem_reads: 0, mem_writes: 0, prints: 0, rets: 0 };
+        let mut cycles = ExecStats {
+            allocs: 0,
+            calls: 0,
+            fast_alu_ops: 0,
+            slow_alu_ops: 0,
+            phis: 0,
+            conditional_branches: 0,
+            unconditional_branches: 0,
+            mem_reads: 0,
+            mem_writes: 0,
+            prints: 0,
+            rets: 0,
+        };
         let result = run_prog(&prog, false, &mut cycles, ExecMode::GC { limit: 100 });
-        assert_eq!(result,Ok(VirtualVal::Data { val: 0 }));
+        assert_eq!(result, Ok(VirtualVal::Data { val: 0 }));
         Ok(())
     }
     #[test]
-    fn check_gctest3() -> Result<(),Box<dyn std::error::Error>>{
+    fn check_gctest3() -> Result<(), Box<dyn std::error::Error>> {
         let bytes = load_program("examples/gctest3.ir")?;
         let prog = parse(&bytes)?;
-        let mut cycles = ExecStats { allocs: 0, calls: 0, fast_alu_ops: 0, slow_alu_ops: 0, phis: 0, conditional_branches: 0, unconditional_branches: 0, mem_reads: 0, mem_writes: 0, prints: 0, rets: 0 };
+        let mut cycles = ExecStats {
+            allocs: 0,
+            calls: 0,
+            fast_alu_ops: 0,
+            slow_alu_ops: 0,
+            phis: 0,
+            conditional_branches: 0,
+            unconditional_branches: 0,
+            mem_reads: 0,
+            mem_writes: 0,
+            prints: 0,
+            rets: 0,
+        };
         let result = run_prog(&prog, false, &mut cycles, ExecMode::GC { limit: 100 });
-        assert_eq!(result,Ok(VirtualVal::Data { val: 4096 }));
+        assert_eq!(result, Ok(VirtualVal::Data { val: 4096 }));
         Ok(())
     }
     #[test]
-    fn check_gctest4() -> Result<(),Box<dyn std::error::Error>>{
+    fn check_gctest4() -> Result<(), Box<dyn std::error::Error>> {
         let bytes = load_program("examples/gctest4.ir")?;
         let prog = parse(&bytes)?;
-        let mut cycles = ExecStats { allocs: 0, calls: 0, fast_alu_ops: 0, slow_alu_ops: 0, phis: 0, conditional_branches: 0, unconditional_branches: 0, mem_reads: 0, mem_writes: 0, prints: 0, rets: 0 };
+        let mut cycles = ExecStats {
+            allocs: 0,
+            calls: 0,
+            fast_alu_ops: 0,
+            slow_alu_ops: 0,
+            phis: 0,
+            conditional_branches: 0,
+            unconditional_branches: 0,
+            mem_reads: 0,
+            mem_writes: 0,
+            prints: 0,
+            rets: 0,
+        };
         let result = run_prog(&prog, false, &mut cycles, ExecMode::GC { limit: 100 });
-        assert_eq!(result,Ok(VirtualVal::Data { val: 4096 }));
+        assert_eq!(result, Ok(VirtualVal::Data { val: 4096 }));
         Ok(())
     }
     #[test]
-    fn check_gctest5() -> Result<(),Box<dyn std::error::Error>>{
+    fn check_gctest5() -> Result<(), Box<dyn std::error::Error>> {
         let bytes = load_program("examples/gctest5.ir")?;
         let prog = parse(&bytes)?;
-        let mut cycles = ExecStats { allocs: 0, calls: 0, fast_alu_ops: 0, slow_alu_ops: 0, phis: 0, conditional_branches: 0, unconditional_branches: 0, mem_reads: 0, mem_writes: 0, prints: 0, rets: 0 };
+        let mut cycles = ExecStats {
+            allocs: 0,
+            calls: 0,
+            fast_alu_ops: 0,
+            slow_alu_ops: 0,
+            phis: 0,
+            conditional_branches: 0,
+            unconditional_branches: 0,
+            mem_reads: 0,
+            mem_writes: 0,
+            prints: 0,
+            rets: 0,
+        };
         let result = run_prog(&prog, false, &mut cycles, ExecMode::GC { limit: 100 });
-        assert_eq!(result,Ok(VirtualVal::Data { val: 4096 }));
+        assert_eq!(result, Ok(VirtualVal::Data { val: 4096 }));
         Ok(())
     }
 }
